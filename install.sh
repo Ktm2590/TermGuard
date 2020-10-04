94efe3d436b96cf9bb1a7e88b0f72d5126d57c11
DarkGa <48998251+DarkGa@users.noreply.github.com>
2020-10-04 22:37:55 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,7 +5,7 @@ if [ $architecture = aarch64 ];
 then cp aarch64/tguard ~/../usr/bin
 chmod +x ~/../usr/bin/tguard
 mkdir ~/../usr/etc/TermGuard
-cp database/database.json ~/../usr/etc/TermGuard
+cp database/database.json ~/../usr/etc/TermGuard/database.json
 echo "Installation on architecture aarch64 completed"
 fi
 termux-open https://t.me/TermGuard
